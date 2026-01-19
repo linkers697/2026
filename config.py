79004f8bde6311584ba6943da312d80613f7b882
linkers697 <music3fhhgyhfgh@gmail.com>
2026-01-19 20:58:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AuraVisual")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Elvexer")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/friends_hindi_chatting_grp")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
@@ -121,6 +121,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
